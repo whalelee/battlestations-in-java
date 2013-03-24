bf9203ffe101073c0846fe6631036b24d1191805
JINQYI LEE <jinqyi.lee@gmail.com>
2013-03-24 11:27:45 +0800

    can display pvp msg

--- src/BattleMenu.java
+++ src/BattleMenu.java
@@ -15,12 +15,16 @@ public class BattleMenu{
 
 	public void fight(int input, ArrayList<Player> targets){
 		Player me = appCtrl.getPlayerLoggedIn();
+		String myName = me.getName();
 		Player target = targets.get(input-1);
+		String targetName = target.getName();
 
-		System.out.print(me.getName() + "'s HP: ");
+		System.out.print(myName + "'s HP: ");
 		System.out.println(me.getCurrentHP() + " / " + me.getTotalHP());
-		System.out.print(target.getName() + "'s HP: ");
+		System.out.print(targetName + "'s HP: ");
 		System.out.println(target.getCurrentHP() + " / " + target.getTotalHP());
+
+		System.out.println(myName + " attacks with xxx at 1240m (xxx damage)");
 	}
 
 }
